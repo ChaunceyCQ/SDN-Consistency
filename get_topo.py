62898acc54a655a3a11c3779890543007c7ed90f
ChaunceyCQ <fancyc7@163.com>
2015-12-24 16:50:37 +0800

    Update get_topo.py

--- get_topo.py
+++ get_topo.py
@@ -32,11 +32,11 @@ UP = 1
 DOWN = 0
 
 
-class SimpleSwitch13(app_manager.RyuApp):
+class GetTopo(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        super(GetTopo, self).__init__(*args, **kwargs)
         # USed for learning switch functioning
         self.mac_to_port = {}
         # Holds the topology data and structure
@@ -67,95 +67,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         datapath = ev.msg.datapath
 
         self.topo_shape.topo_switches[datapath_id]=[datapath]
-        
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions)
 
-    def delete_flow(self, datapath):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        for dst in self.mac_to_port[datapath.id].keys():
-            match = parser.OFPMatch(eth_dst=dst)
-            mod = parser.OFPFlowMod(
-                datapath, command=ofproto.OFPFC_DELETE,
-                out_port=ofproto.OFPP_ANY, out_group=ofproto.OFPG_ANY,
-                priority=1, match=match)
-            datapath.send_msg(mod)
-
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
-        datapath.send_msg(mod)
-
-    """
-    This is called when Ryu receives an OpenFlow packet_in message. The trick is set_ev_cls decorator. This decorator
-    tells Ryu when the decorated function should be called.
-    """
-    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
-        if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
-        msg = ev.msg
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
-
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-
-        dst = eth.dst
-        src = eth.src
-
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
-        #self.logger.info("\tpacket in %s %s %s %s", dpid, src, dst, in_port)
-
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
-
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
-
-        actions = [parser.OFPActionOutput(out_port)]
-        #actions_to_controller = actions
-
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
-            # verify if we have a valid buffer_id, if yes avoid to send both
-            # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
-            else:
-                self.add_flow(datapath, 1, match, actions)
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        out= parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                 in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
 
     ###################################################################################
     """
@@ -312,70 +224,6 @@ class TopoStructure():
                     (link.src.dpid, link.src.port_no), (link.dst.dpid, link.dst.port_no)):
                 return True
         return False
-
-    """
-    Finds the shortest path from source s to all other nodes.
-    Both s and d are switches.
-    """
-    def find_shortest_path(self, s):
-        # I really recommend watching this video: https://www.youtube.com/watch?v=zXfDYaahsNA
-        s_count = self.switches_count()
-        s_temp = s
-
-        # If you wanna see the prinfs set this to one.
-        verbose = 0
-
-        visited = []
-
-        Fereng = []
-        Fereng.append(s_temp)
-
-        # Records number of hubs which you can reach the node from specified src
-        shortest_path_hubs = {}
-        # The last node which you can access the node from. For example: {1,2} means you can reach node 1 from node 2.
-        shortest_path_node = {}
-        shortest_path_hubs[s_temp] = 0
-        shortest_path_node[s_temp] = s_temp
-        while s_count > len(visited):
-            if verbose == 1: print "visited in: " + str(visited)
-            visited.append(s_temp)
-            if verbose == 1: print ("Fereng in: " + str(Fereng))
-            if verbose == 1: print ("s_temp in: " + str(s_temp))
-            for l in self.find_links_with_src(s_temp):
-                if verbose == 1: print "\t" + str(l)
-                if l.dst.dpid not in visited:
-                    Fereng.append(l.dst.dpid)
-                if verbose == 1: print ("\tAdded {0} to Fereng: ".format(l.dst.dpid))
-                if l.dst.dpid in shortest_path_hubs:
-                    # Find the minimum o
-                    if shortest_path_hubs[l.src.dpid] + 1 < shortest_path_hubs[l.dst.dpid]:
-                        shortest_path_hubs[l.dst.dpid] = shortest_path_hubs[l.src.dpid] + 1
-                        shortest_path_node[l.dst.dpid] = l.src.dpid
-                    else:
-                        shortest_path_hubs[l.dst.dpid] = shortest_path_hubs[l.dst.dpid]
-
-                    if verbose == 1: print(
-                        "\t\tdst dpid found in shortest_path. Count: " + str(shortest_path_hubs[l.dst.dpid]))
-                elif l.src.dpid in shortest_path_hubs and l.dst.dpid not in shortest_path_hubs:
-                    if verbose == 1: print("\t\tdst dpid not found bit src dpid found.")
-                    shortest_path_hubs[l.dst.dpid] = shortest_path_hubs[l.src.dpid] + 1
-                    shortest_path_node[l.dst.dpid] = l.src.dpid
-            if verbose == 1:
-                print ("shortest_path Hubs: " + str(shortest_path_hubs))
-                print ("shortest_path Node: " + str(shortest_path_node))
-            if s_temp in Fereng:
-                Fereng.remove(s_temp)
-            #min_val = min(Fereng)
-            if verbose == 1: print ("Fereng out: " + str(Fereng))
-            t_dpid = [k for k in Fereng if k not in visited]
-            if verbose == 1: print ("Next possible dpids (t_dpid): " + str(t_dpid))
-
-            if len(t_dpid) != 0:
-                s_temp = t_dpid[t_dpid.index(min(t_dpid))]
-
-            if verbose == 1: print "s_temp out: " + str(s_temp)
-            if verbose == 1: print "visited out: " + str(visited) + "\n"
-        return shortest_path_hubs, shortest_path_node
 
     """
     Find a path between src and dst based on the shorted path info which is stored on shortest_path_node
